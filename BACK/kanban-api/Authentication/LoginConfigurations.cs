81171f5695cdf1e7e40093d80ada8fbef95c0f78
Bruno Bragança <bruno.braganca@outlook.com.br>
2022-03-28 16:52:04 -0300

    feature 3
    Alterações no login, para usar variável de ambiente e , injeção de dependência para validar o login
    
    Remoção de código não utilizado

---
+++ BACK/kanban-api/Authentication/LoginConfigurations.cs
@@ -0,0 +1,8 @@
+﻿namespace kanban_api.Authentication
+{
+    public class LoginConfigurations
+    {
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
--- BACK/kanban-api/BusinessLayer/LoginBL.cs
+++ BACK/kanban-api/BusinessLayer/LoginBL.cs
@@ -1,16 +1,25 @@
-﻿using kanban_api.Utils;
+﻿using kanban_api.Authentication;
+using kanban_api.Utils;
 
 namespace kanban_api.BusinessLayer
 {
     public class LoginBL
     {
+        private readonly LoginConfigurations _loginConfiguration;
+        public LoginBL(LoginConfigurations loginConfiguration)
+        {
+            _loginConfiguration = loginConfiguration;
+        }
         public void ValidateParameters(string username, string password)
         {
             var validateModel = ValidateModel.Start();
 
-            validateModel.Fail(string.IsNullOrWhiteSpace(username), "Username deve ser preenchido.")
-                .Fail(string.IsNullOrWhiteSpace(password), "Senha deve ser preenchida.")
+            bool failUsername = string.IsNullOrWhiteSpace(username) || !string.Equals(username, _loginConfiguration.Login, StringComparison.InvariantCultureIgnoreCase);
+            bool failPassword = string.IsNullOrWhiteSpace(password) || !string.Equals(password, _loginConfiguration.Senha, StringComparison.InvariantCultureIgnoreCase);
+            validateModel.Fail(failUsername, "Login vazio ou inválido.")
+                .Fail(failPassword, "Senha vazia ou inválida.")
                 .Validate(StatusCodes.Status400BadRequest);
         }
     }
 }
+;
