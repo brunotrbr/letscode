5dd36c258a13ec3722337d65aaed818a44870b85
Bruno Bragança <bruno.braganca@outlook.com.br>
2022-03-28 12:25:00 -0300

    Feature 1 Criação de token JWT e devolução do mesmo

---
+++ BACK/kanban-api/BusinessLayer/LoginBL.cs
@@ -0,0 +1,16 @@
+﻿using kanban_api.Utils;
+
+namespace kanban_api.BusinessLayer
+{
+    public class LoginBL
+    {
+        public void ValidateParameters(string username, string password)
+        {
+            var validateModel = ValidateModel.Start();
+
+            validateModel.Fail(string.IsNullOrWhiteSpace(username), "Username deve ser preenchido.")
+                .Fail(string.IsNullOrWhiteSpace(password), "Senha deve ser preenchida.")
+                .Validate(StatusCodes.Status400BadRequest);
+        }
+    }
+}
