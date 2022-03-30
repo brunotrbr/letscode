ae771b96b48615fd0d6c2bd5cc58d9d47a067fc4
Bruno Trindade Bragança <13382484+bruno-braganca@users.noreply.github.com>
2022-03-30 03:44:18 -0300

    Update build.sh
    Correção no comando cd do build

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo "Build dos Dockerfiles do backend e do frontend"
 export DOCKER_BUILDKIT=1
 cd BACK/kanban-api
 docker build -t kanban_api .
-CD ../../FRONT
+cd ../../FRONT
 docker build -t kanban_frontend .
 
 echo "Imagens buildadas, executar docker-compose up"
