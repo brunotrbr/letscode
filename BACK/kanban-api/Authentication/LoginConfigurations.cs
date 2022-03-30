3ea3d748bfe6b5f4250f5db064f0d88374ce4679
Bruno Bragança <bruno.braganca@outlook.com.br>
2022-03-30 01:37:54 -0300

    Remoção final do kanban-api

--- BACK/kanban-api/Authentication/LoginConfigurations.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace kanban_api.Authentication
-{
-    public class LoginConfigurations
-    {
-        public string Login { get; set; }
-        public string Senha { get; set; }
-    }
-}
--- BACK/kanban-api/Models/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace kanban_api.Models
-{
-    public class User
-    {
-        public string Login { get; set; }
-        public string Senha { get; set; }
-    }
-}
